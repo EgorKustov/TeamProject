9c9a063a49a1a2175cfc8ee8dfc823c831971c34
Egor Kustov <egorkustov@icloud.com>
2023-11-29 11:05:56 +0300

    Fix Bugs

--- src/main/java/ru/netology/javaqadiplom/Bank.java
+++ src/main/java/ru/netology/javaqadiplom/Bank.java
@@ -8,8 +8,9 @@ public class Bank {
      * уменьшиться на эту сумму, а баланс счёта to увеличиться.
      * Если операция прошла неуспешно, балансы обоих счетов никак
      * измениться не должны.
-     * @param from - счёт с которого переводим
-     * @param to - счёт на который переводим
+     *
+     * @param from   - счёт с которого переводим
+     * @param to     - счёт на который переводим
      * @param amount - сумма перевода
      * @return - true если операция прошла успешно, false иначе
      */
@@ -17,9 +18,31 @@ public class Bank {
         if (amount <= 0) {
             return false;
         }
-        if (from.pay(amount)) {
-            to.add(amount);
+
+        if (from instanceof CreditAccount) {
+            CreditAccount creditAccount = (CreditAccount) from;
+            if (amount > creditAccount.getCreditLimit()) {
+                return false;
+            }
         }
+
+        if (from.pay(amount)) {
+            if (to.add(amount)) {
+                if (to instanceof SavingAccount) {
+                    SavingAccount savingAccount = (SavingAccount) to;
+                    if (savingAccount.getBalance() > savingAccount.getMaxBalance() || savingAccount.getBalance() < savingAccount.getMinBalance()) {
+                        // Откатываем операцию только при нарушении условий SavingAccount
+                        from.add(amount);
+                        to.pay(amount);
+                        return false;
+                    }
+                }
+            } else {
+                from.add(amount); // Откатываем операцию при неудачном зачислении на счет to
+                return false;
+            }
+        }
+
         return true;
     }
 }
